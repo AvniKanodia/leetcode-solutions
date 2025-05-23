4553a5c7d4c7e443b6c0f70940f99799d074a556
Avni Kanodia <125621133+AvniKanodia@users.noreply.github.com>
2025-05-23 20:49:18 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- my-folder/problems/two_sum/solution.cpp
+++ my-folder/problems/two_sum/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //create map
         map<int,int> map;
         for (int i = 0; i < nums.size(); i++) {
             int comp = target - nums[i];
