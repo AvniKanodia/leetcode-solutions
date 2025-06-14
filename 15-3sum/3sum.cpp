2d4718c45d5c91601aa221b7755e0e24e209c246
Avni Kanodia <125621133+AvniKanodia@users.noreply.github.com>
2025-06-14 12:35:15 -0500

    Time: 35 ms (98.60%) | Memory: 29 MB (90.67%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,23 +5,27 @@ public:
         if (nums.size() < 3) return {};
         if ( nums[0] > 0) return {};
 
-        map<int, int> seen;
         vector<vector<int>> res;
-        for(int i = 0 ; i < nums.size() ; ++i){   
-            seen[nums[i]] = i;
-        }
+        int n = nums.size();
 
-        for (int i = 0; i < nums.size()-2; ++i) {
+        for (int i = 0; i < n - 2; ++i) {
             if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
 
-            for (int j = i+1; j < nums.size()-1; ++j) {
-                int target = -1 * (nums[i] + nums[j]);
-                if (seen.count(target) && seen[target] > j) {
-                    res.push_back({nums[i], nums[j], target});
+            int low = i + 1;
+            int high = n - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum > 0) high--;
+                else if (sum < 0) low++;
+                else {
+                    res.push_back({nums[i], nums[low], nums[high]});
+                    low++;
+                    high--;
+                    while (low < high && nums[low] == nums[low-1]) low++;
+                    while (low < high && nums[high] == nums[high+1]) high--;
                 }
-                j = seen[nums[j]];
             }
-            i = seen[nums[i]];
         }
         return res;
     }
