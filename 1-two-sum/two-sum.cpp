b1821faafe7a37f20c7649236a941e6f8b8e8f83
Avni Kanodia <125621133+AvniKanodia@users.noreply.github.com>
2025-06-17 22:13:46 -0500

    Time: 6 ms (47.50%) | Memory: 16.3 MB (7.26%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      map<int, int> map;
+      for (int i = 0; i < nums.size(); ++i) map.insert({nums[i],i});
+
+      for (int i = 0; i < nums.size(); ++i) {
+        int comp = target - nums[i];
+        if (map.count(comp) && map[comp] != i) return {i, map[comp]};
+      }
+    
+    return {};
+    }
+};
